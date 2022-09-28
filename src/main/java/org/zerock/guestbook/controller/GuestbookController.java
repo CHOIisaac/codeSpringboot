798106691b208be9f2fa2fad53c50d0aae27b4d7
CHOIisaac <ehdrhkdls@gmail.com>
2022-09-28 22:02:48 +0900

    part2 방명록 조회처리

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
@@ -37,7 +35,7 @@ public class GuestbookController {
         log.info("register get...");
     }
 
-    @PatchMapping("/register")
+    @PostMapping("/register")
     public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
         log.info("dto... "+dto);
 
@@ -46,5 +44,14 @@ public class GuestbookController {
         redirectAttributes.addFlashAttribute("msg", gno);
 
         return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+        public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model){
+            log.info("gno: "+gno);
+
+            GuestbookDTO dto = service.read(gno);
+
+            model.addAttribute("dto", dto);
     }
 }
